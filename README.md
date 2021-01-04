c346dc5635f7964f704b4beb2b1fd8750f381dca
Romain Clement <contact@romain-clement.net>
2021-01-04 22:07:44 +0100

    Initial commit
    Import from griffonnage/griffonnage-sync

---
+++ README.md
@@ -0,0 +1,75 @@
+# Ephemere - Sync Server
+
+> Open a secure line
+
+Synchronize end-to-end encrypted and ephemeral messages from
+Ephemere using [Socket-io](https://socket.io).
+
+## Build Setup
+
+```
+// install dependencies and post-build app
+$ npm install
+
+// run in development mode
+$ npm run dev
+
+// run in production mode
+$ npm run build
+$ npm run start
+```
+
+## Docker
+
+### Build
+
+```
+docker build -t ephemere/ephemere-sync:latest .
+```
+
+### Run
+
+```
+docker run --rm --env-file .env -p 9000:9000 ephemere/ephemere-sync:latest
+```
+
+## Scaling
+
+Some technical requirements are involved if you plan on deploying this
+synchronization server as a cluster of nodes.
+
+Please referer to the [Socket-IO Documentation]((https://socket.io/docs/using-multiple-nodes/))
+on the matter for extensive explanations.
+
+### Sticky sessions
+
+Due to some long-polling operations required by Socket-IO,
+you need a "sticky session" mechanism in your load-balancing system.
+
+For instance, if using Heroku you must enable their
+[Session Affinity](https://devcenter.heroku.com/articles/session-affinity) feature
+for your app:
+
+```
+$ heroku features:enable http-session-affinity -a <app-id>
+$ heroku features -a <app-id>
+```
+
+### Message broker
+
+When scaling Socker-IO to multiple nodes, a message broker is required in order
+to pass messages between nodes seamlessly. Fortunately, Socker-IO as an "adapter"
+mechanism and an official [Redis Adapter](https://github.com/socketio/socket.io-redis).
+
+The Redis adapter is already integrated into this app, but disabled by default.
+To enable it, simply set the `REDIS_URI` environment variable:
+
+```
+REDIS_URI=redis://localhost:6379/0
+```
+
+## License
+
+Licensed under GNU Affero General Public License v3.0 (AGPLv3)
+
+Copyright (c) 2021 - present Romain Clement
---
+++ src/index.ts
@@ -0,0 +1,78 @@
+import http from 'http'
+import express from 'express'
+import { Server } from 'socket.io'
+import { createAdapter } from 'socket.io-redis'
+import pino from 'pino'
+import pinoHttp from 'pino-http'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+enum Events {
+  userJoin = 'user-join',
+  userLeave = 'user-leave',
+  userList = 'user-list',
+  newRoomData = 'new-room-data',
+}
+
+const port = process.env.PORT || 80
+const corsOrigin = process.env.CORS_ORIGIN || undefined
+const redisUri = process.env.REDIS_URI || undefined
+
+const logger = pino()
+
+const app = express()
+app.use((req, res, next) => {
+  if (req.header('X-CleverCloud-Monitoring') === 'telegraf') {
+    return res.sendStatus(200)
+  }
+  next()
+})
+app.use(pinoHttp())
+
+const server = http.createServer(app)
+server.listen(port, () => {
+  logger.info(`Server started on port ${port}`)
+})
+
+const io = new Server(server, {
+  cors: {
+    origin: corsOrigin,
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+})
+
+if (redisUri) {
+  logger.info('Enabling Socket-IO Redis adapter')
+  io.adapter(createAdapter(redisUri))
+}
+
+io.on('connection', async (socket) => {
+  socket.on('join-room', async (room: string) => {
+    socket.join(room)
+    socket.to(room).emit(Events.userJoin, socket.id)
+
+    const ids = await io.in(room).allSockets()
+    io.in(room).emit(Events.userList, [...ids])
+  })
+
+  socket.on('leave-room', async (room: string) => {
+    socket.leave(room)
+    socket.to(room).emit(Events.userLeave, socket.id)
+
+    const ids = await io.in(room).allSockets()
+    io.in(room).emit(Events.userList, [...ids])
+  })
+
+  socket.on('broadcast-room-data', (room: string, encryptedData: string) => {
+    socket.to(room).emit(Events.newRoomData, encryptedData)
+  })
+
+  socket.on(
+    'broadcast-volatile-room-data',
+    (room: string, encryptedData: string) => {
+      socket.volatile.to(room).emit(Events.newRoomData, encryptedData)
+    }
+  )
+})
